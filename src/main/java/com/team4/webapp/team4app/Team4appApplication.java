f117cafc08d72df167367c94dc6e76017d1e0821
Arghya <arghya_2005@outlook.com>
2019-01-01 10:40:12 +0530

    new commit
    new commit

---
+++ src/main/java/com/team4/webapp/team4app/Team4appApplication.java
@@ -0,0 +1,12 @@
+package com.team4.webapp.team4app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team4appApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team4appApplication.class, args);
+	}
+}
---
+++ src/test/java/com/team4/webapp/team4app/Team4appApplicationTests.java
@@ -0,0 +1,16 @@
+package com.team4.webapp.team4app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Team4appApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
